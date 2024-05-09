b4f6a23a4f202087378e0f7350340cc75a1f0814
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-05-08 17:49:17 -0700

    🎨 Create components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'dark-purple': '#24053E',
+			'davy-grey': '#584D62',
+			'eucaplyptus': '#44FFA1',
+			'ghost-white': '#FCF8FF',
+			'white': '#FFFFFF'
+		},
+		fontFamily: {
+			'manrope': ['Manrope Variable', 'sans-serif'],
+			'fraunces': ['Fraunces Variable', 'serif']
+		}
 	},
 	plugins: [],
 }
